6720c5c31f8c5efc01ed1f6291abf54a4f217315
Udaykiran1817 <playbolduday895@gmail.com>
2026-05-10 17:29:48 +0530

    added dutch_color_pattern

---
+++ ColorProblem.java
@@ -0,0 +1,38 @@
+public class ColorProblem {
+   public static void main(String[] args) {
+      int[] nums = { 1, 0, 1, 0, 1, 2, 2, 0, 1, 0, 1, 1, 0, 0, 2, 2, 1, 0, 1, 0, 2, 2, 0, 1, 0 };
+
+      Demo.test(nums);
+      for (int num : nums) {
+         System.out.print(num + " ");
+      }
+
+   }
+}
+
+class Demo {
+   public static void test(int[] nums) {
+      int low = 0;
+      int mid = 0;
+      int high = nums.length - 1;
+
+      while (mid <= high) {
+         if (nums[mid] == 0) {
+            int temp = nums[low];
+            nums[low] = nums[mid];
+            nums[mid] = temp;
+
+            low++;
+            mid++;
+         } else if (nums[mid] == 1) {
+            mid++;
+         } else {
+            int temp = nums[mid];
+            nums[mid] = nums[high];
+            nums[high] = temp;
+
+            high--;
+         }
+      }
+   }
+}
